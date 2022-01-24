e511cfeb58778d93925eb4005ecbb96d585943d6
alexandrov <andrey.aleksandrov@gmail.com>
2022-01-24 08:55:09 +0300

    init base

--- examples/main.py
+++ examples/main.py
@@ -20,18 +20,7 @@ def test_metrics(mon: Monitoring, n):
 
 if __name__ == '__main__':
 
-    _mon0 = Monitoring(
-        credentials={
-            "service_account_key": {
-                "service_account_id": SERVICE_ACCOUNT_ID,
-                "id": SERVICE_ACCOUNT_KEY_ID,
-                "private_key": open(os.path.join(os.path.dirname(__file__), "private.key"), "rb").read().decode()
-            },
-            "cloudId": CLOUD_ID,
-            "folderId": FOLDER_ID
-        }
-    )
-    _mon1 = Monitoring(
+    _mon = Monitoring(
         credentials={
             "service_account_key": {
                 "service_account_id": SERVICE_ACCOUNT_ID,
@@ -44,19 +33,7 @@ if __name__ == '__main__':
         workers=1
     )
 
-    #DGAUGE: Числовой показатель. Задается дробным числом.
-    #IGAUGE: Числовой показатель. Задается целым числом.
-    #COUNTER: Счетчик.
-    #RATE: Производная.
-
-    _ptns = time.process_time_ns()
-    _tns = time.time_ns()
-    for n in range(100):
-        with Chrono(_mon1, labels={"with": "workers"}, mul=1):
-            test_metrics(_mon0, n)
-    print((time.process_time_ns() - _ptns) / 10 ** 9, (time.time_ns() - _tns) / 10 ** 9)
-
-    # 0.122077 0.758518  elements=100, period = 10, workers = 0 (default)
-    # 1.05552 65.05855 elements=1, period = 10, workers = 0
-    # ~ 0.045114 0.043375 elements=100, period = 10, workers = 1 with multiprocessing
-
+    for n in range(1000):
+        with Chrono(_mon, labels={"with": "workers"}, mul=10**3):
+            test_metrics(_mon, n)
+            time.sleep(0.3)
